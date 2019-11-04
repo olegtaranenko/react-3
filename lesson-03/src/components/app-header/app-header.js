febfdc7dac2d1514fecf2fa93dd6efc6ac55fd77
Oleg Taranenko <olegtaranenko@gmail.com>
2019-11-04 22:43:46 +0100

    wrong text for list item

--- lesson-03/src/components/app-header/app-header.js
+++ lesson-03/src/components/app-header/app-header.js
@@ -5,7 +5,7 @@ const AppHeader = () => {
   return (
     <div className="app-header d-flex">
       <h1>Oleg Taranenko</h1>
-      <h2>5 записей, из них понравилось</h2>
+      <h2>5 записей, из них понравилось 0</h2>
     </div>
   )
 };
