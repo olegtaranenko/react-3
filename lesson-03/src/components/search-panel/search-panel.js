0b2fbb871501f27ff64e0927de6f6ec90619ecc5
Oleg Taranenko <olegtaranenko@gmail.com>
2019-11-04 20:44:25 +0100

    lesson 3 main done

---
+++ lesson-03/src/components/search-panel/search-panel.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './search-panel.css'
+
+const SearchPanel = () => {
+  return (
+    <input
+      className="form-control search-import"
+      type="text"
+      placeholder="Поиск по записям"
+    />
+  )
+};
+
+export default SearchPanel
