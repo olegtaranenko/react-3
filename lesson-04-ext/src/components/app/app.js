ae8777b2e352705edd04cad026123145272d34f0
Oleg Taranenko <olegtaranenko@gmail.com>
2019-11-05 15:03:26 +0100

    clone lesson 4 -> lesson 4 ext

---
+++ lesson-04-ext/src/components/app/app.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import AppHeader        from "../app-header";
+import SearchPanel      from "../search-panel";
+import PostStatusFilter from "../post-status-filter";
+import PostList         from "../post-list";
+import PostAddForm      from "../post-add-form";
+
+import './app.css'
+
+const App = () => {
+
+  const data = [
+    5,
+    null,
+    {label: 'should not be viewable, missed id'},
+    {id: 'missed label'},
+    // {label: 'that\'s ok', id: 1234},
+    {label: "Going to learn React", important: true, id: 1},
+    {label: "That is so good", important: false, id: 2},
+    {label: "I need a break...", important: false, id: 3},
+  ];
+  return (
+    <div className="app">
+      <AppHeader/>
+      <div className="search-panel d-flex">
+        <SearchPanel/>
+        <PostStatusFilter/>
+      </div>
+      <PostList posts={data}/>
+      <PostAddForm/>
+    </div>
+  )
+};
+
+export default App
