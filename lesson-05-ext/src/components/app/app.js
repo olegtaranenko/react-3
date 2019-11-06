d96af979302784a23e0f74ebd7ebc71e8a80ee63
Oleg Taranenko <olegtaranenko@gmail.com>
2019-11-06 22:19:48 +0100

    Урок №5 усложненный готов: свой генератор случайных чисел, подтверждение удаления записи

--- lesson-05-ext/src/components/app/app.js
+++ lesson-05-ext/src/components/app/app.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import styled from "styled-components";
-import nextId from "react-id-generator";
+import Generator from "./id-generator";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-import AppHeader   from "../app-header";
+import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import PostList    from "../post-list";
 import PostAddForm from "../post-add-form";
 
+const generator = new Generator({
+  maxValue: 1e5,
+  // prefix: 'id-'
+});
 
 const AppBlock = styled.div`
   margin: 0 auto;
@@ -14,20 +19,36 @@ const AppBlock = styled.div`
 `;
 
 export default class App extends Component {
+
   constructor(props) {
     super(props);
     const {data} = props;
 
     this.state = {
-      data: data.filter(post => post && typeof post === 'object' && post.id && post.label)
+      data: data.filter(post => post && typeof post === 'object' && post.id && post.label),
+      modal: false
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
-    // this.maxId = 4;
+    this.runModal = this.runModal.bind(this);
+    this.setModal = this.setModal.bind(this);
   }
 
-  deleteItem(id) {
-    console.log(id);
+
+  runModal(id) {
+    console.log('about to delete %s', id);
+    this.setModal(true);
+    this.setState(({idToDelete}) => {
+      return {idToDelete: id}
+    })
+  }
+
+
+  deleteItem() {
+    let id = this.state.idToDelete;
+    console.log('do delete %s', id);
+    this.setModal(false);
+
     this.setState(({data}) => {
       const index = data.findIndex(elem => elem.id === id);
       const before = data.slice(0, index);
@@ -39,7 +60,7 @@ export default class App extends Component {
   }
 
   addItem(body) {
-    let id = nextId();
+    let id = generator.get();
     console.log(id);
 
     const newItem = {
@@ -55,19 +76,44 @@ export default class App extends Component {
     });
   };
 
+  setModal(visible) {
+    this.setState(() => {
+      return {modal: visible}
+    });
+  }
+
+
   render() {
+    const toggle = () => {
+      this.setState(({modal}) => {
+        return {modal: !modal}
+      });
+    };
+
     return (
-      <AppBlock>
-        <AppHeader/>
-        <SearchPanel/>
-        <PostList
-          posts={this.state.data}
-          onDelete={this.deleteItem}
-        />
-        <PostAddForm
-          onAdd={this.addItem}
-        />
-      </AppBlock>
+       <div>
+        <AppBlock>
+          <AppHeader/>
+          <SearchPanel/>
+          <PostList
+            posts={this.state.data}
+            onDelete={this.runModal}
+          />
+          <PostAddForm
+            onAdd={this.addItem}
+          />
+        </AppBlock>
+
+        <Modal isOpen={this.state.modal} toggle={toggle}>
+          <ModalHeader toggle={toggle}>Попытка удалить пост</ModalHeader>
+          <ModalBody>Вы точно собираетесь удалить запись?</ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.deleteItem}>Да!</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Отмена</Button>
+          </ModalFooter>
+        </Modal>
+
+      </div>
     )
   }
 };
