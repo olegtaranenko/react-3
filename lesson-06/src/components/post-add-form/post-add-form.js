5495ece084d94ef05fefe748f86ef734e55e5df7
Oleg Taranenko <olegtaranenko@gmail.com>
2019-11-07 22:22:55 +0100

    spaces only added as post

--- lesson-06/src/components/post-add-form/post-add-form.js
+++ lesson-06/src/components/post-add-form/post-add-form.js
@@ -30,7 +30,9 @@ export default class PostAddForm extends Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-    const {text} = this.state;
+    let {text} = this.state;
+    text = text.trim();
+
     if (text) {
       this.props.onAdd(text);
       this.setState({
