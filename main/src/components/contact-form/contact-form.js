dcfa7cc1067b86ccde5c8c3033272d341f939b1a
Oleg Taranenko <olegtaranenko@gmail.com>
2019-12-06 20:52:25 +0100

    add spinner to Contact Us form

--- main/src/components/contact-form/contact-form.js
+++ main/src/components/contact-form/contact-form.js
@@ -1,12 +1,10 @@
 import React, {Component} from 'react';
 import * as Yup           from 'yup';
-import {connect}          from 'react-redux';
 import {withRouter}       from 'react-router-dom';
 
-import {ErrorMessage, Field, Form, Formik}                  from 'formik';
-import WithShopService                                      from "../with-shop-service";
-import {parsePhoneNumberFromString}                         from 'libphonenumber-js';
-import {contentLoaded, contentRequested, shopServiceFailed} from "../../actions";
+import {ErrorMessage, Field, Form, Formik} from 'formik';
+import {parsePhoneNumberFromString}        from 'libphonenumber-js';
+import WithShopService                     from "../with-shop-service";
 
 // import detectBrowserLanguage from 'detect-browser-language';
 
@@ -52,59 +50,65 @@ class ContactForm extends Component {
     const {ShopService} = this.props;
 
     const MyFormik = withRouter(({history}) => (
-          <Formik
-            initialValues={{
-              name:    '',
-              email:   '',
-              phone:   '',
-              message: ''
-            }}
-            validationSchema={yupContactSchema}
-            onSubmit={(values, {setSubmitting}) => {
-              setTimeout(() => {
-                ShopService.saveMessage(values);
-                setSubmitting(false);
-                history.push('/thanks');
-              }, 2000);
-            }}
-          >
-            {({isSubmitting}) => (
-              <Form className="contact-form">
-                <ContactField
-                  type="name"
-                  name="name"
-                  label="Name"
-                  placeholder="Enter your Name"
-                />
-                <ContactField
-                  type="email"
-                  name="email"
-                  label="Email"
-                  placeholder="Enter your E-Mail"
-                />
-                <ContactField
-                  type="phone"
-                  name="phone"
-                  label="Phone"
-                  placeholder="Enter phone number"
-                  required={false}
-                  // value={numberToShow}
-                />
-                <ContactField
-                  as="textarea"
-                  name="message"
-                  label="Your message"
-                  placeholder="Tell us..."
-                  labelAlign="up"
-                  theme="message"
-                />
-                <button className="contact-btn contact-form__submit" type="submit" disabled={isSubmitting}>
-                  Send us
-                </button>
-              </Form>
-            )}
-          </Formik>
-
+      <Formik
+        initialValues={{
+          name:    '',
+          email:   '',
+          phone:   '',
+          message: ''
+        }}
+        validationSchema={yupContactSchema}
+        onSubmit={(values, {setSubmitting}) => {
+          setTimeout(() => {
+            ShopService.saveMessage(values);
+            setSubmitting(false);
+            history.push('/thanks');
+          }, 1000);
+        }}
+      >
+        {({isSubmitting}) => {
+          let loaderClassName = "contact-form__spinner";
+          if (isSubmitting) {
+            loaderClassName += ' active'
+          }
+          return <Form className="contact-form">
+            <ContactField
+              type="name"
+              name="name"
+              label="Name"
+              placeholder="Enter your Name"
+            />
+            <ContactField
+              type="email"
+              name="email"
+              label="Email"
+              placeholder="Enter your E-Mail"
+            />
+            <ContactField
+              type="phone"
+              name="phone"
+              label="Phone"
+              placeholder="Enter phone number"
+              required={false}
+              // value={numberToShow}
+            />
+            <ContactField
+              as="textarea"
+              name="message"
+              label="Your message"
+              placeholder="Tell us..."
+              labelAlign="up"
+              theme="message"
+            />
+            <button className="contact-btn contact-form__submit" type="submit" disabled={isSubmitting}>
+              Send us
+            </button>
+            <div className={loaderClassName}>
+              <img src="../img/Eclipse-1s-64px.svg" alt="spinner"/>
+            </div>
+          </Form>
+        }}
+      </Formik>
     ));
 
     return (
@@ -162,13 +166,8 @@ const ContactField = ({
     </div>
   )
 };
-const mapDispatchToProps = {
-  contentRequested,
-  contentLoaded,
-  shopServiceFailed
-};
 
-export default WithShopService()(connect(undefined, mapDispatchToProps)(ContactForm));
+export default WithShopService()(ContactForm);
 
 
 /*
