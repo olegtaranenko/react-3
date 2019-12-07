b4d4705a7a044955633f76ca4148962fe308e8f2
Oleg Taranenko <olegtaranenko@gmail.com>
2019-12-07 17:39:42 +0100

    remove comments

--- main/src/components/contact-form/contact-form.js
+++ main/src/components/contact-form/contact-form.js
@@ -4,10 +4,9 @@ import {withRouter}       from 'react-router-dom';
 import InputMask          from 'react-input-mask';
 
 import {ErrorMessage, Field, Form, Formik} from 'formik';
-// import {parsePhoneNumberFromString}        from 'libphonenumber-js';
+
 import WithShopService                     from "../with-shop-service";
 
-// import detectBrowserLanguage from 'detect-browser-language';
 
 
 class ContactForm extends Component {
@@ -19,26 +18,6 @@ class ContactForm extends Component {
                .min(2, 'Too Short!')
                .max(50, 'Too Long!')
                .required('Required'),
-/*
-      phone:   Yup.string()
-               .min(7)
-               .test('phone', 'Phone number is not valid', function (value) {
-                 // noinspection EqualityComparisonWithCoercionJS
-                 if (!value && value != 0) {
-                   return true;
-                 }
-
-                 let ret = false;
-                 try {
-                   detectedNumber = parsePhoneNumberFromString(value);
-                   return !!detectedNumber.number
-                 } catch (e) {
-                   // do nothing
-                 }
-
-                 return ret;
-               }),
-*/
       email:   Yup.string()
                .email('Invalid email')
                .min(5, 'Too Short!')
@@ -48,7 +27,6 @@ class ContactForm extends Component {
                .required('Required')
     });
 
-    // const numberToShow = detectedNumber ? detectedNumber.formatInternational() : null;
     const {ShopService} = this.props;
 
     const MyFormik = withRouter(({history}) => (
@@ -191,56 +169,3 @@ const ContactField = ({
 };
 
 export default WithShopService()(ContactForm);
-
-
-/*
-    const browserLanguage = detectBrowserLanguage();
-    // const preferredCountries = [];
-    const usedCountries = {};
-
-    if (browserLanguage) {
-      const splitter = browserLanguage.match(/([a-z]{2})?_?([A-Z]{2})/);
-      const country = splitter[2];
-      if (country) {
-        preferredCountries.push(country);
-        usedCountries[country] = true;
-      }
-    }
-
-
-    ['RU', 'DE', 'US', 'UK', 'UA', 'BY', 'KZ', 'LT'].forEach(country => {
-      if (!usedCountries[country]) {
-        preferredCountries.push(country)
-      }
-    });
-*/
-// console.log(preferredCountries);
-
-// import {PhoneNumberUtil, PhoneNumberFormat as PNF}                   from 'google-libphonenumber'
-/*
-     preferredCountries.some(country => {
-       try {
-         const number = pnu.parseAndKeepRawInput(value, country);
-         // console.log(pnu.format(number, PNF.INTERNATIONAL));
-         const isPossible = pnu.isPossibleNumber(number);
-         let countryCode;
-         if (isPossible) {
-           countryCode = number.getCountryCode();
-         }
-         let isValid = pnu.isValidNumberForRegion(number, country);
-
-         if (!isValid && countryCode) {
-           isValid = pnu.isValidNumber(number);
-         }
-         if (isValid) {
-           detectedNumber = number;
-           ret = isValid
-         }
-         return ret;
-       } catch (e) {
-         // do nothing
-         console.log(`${value} is not a phone number`);
-       }
-       return ret;
-     });
-*/
